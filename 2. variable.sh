2de9d5cfed3af01b00f98dc3b87a9852695de9a1
NiBienvenu <nicibienvenu@gmail.com>
2024-09-28 05:51:52 +0200

    variable

---
+++ 2. variable.sh
@@ -0,0 +1,22 @@
+#!/bib/bash
+
+#variable : is a value that can change, depending on conditions or on information passed to the program .
+# A Variable in bash can contain a number ,a character,or a string for character
+
+
+# Dans le langage Bash, 
+# une variable est un moyen de stocker des données sous un nom donné pour pouvoir les utiliser plus tard dans un script ou une session. 
+# Les variables peuvent contenir différents types de données, comme des chaînes de caractères, des nombres, ou des résultats de commandes.
+
+# Pour créer une variable, il suffit de l'assigner sans espaces autour du signe = :
+
+# Exemple :
+ma_variable="Bonjour, le monde!"
+
+# Pour afficher une variable dans le terminal, on utilise la commande echo et Pour utiliser la valeur d'une variable, on la précède d'un $ :
+echo $ma_variable
+
+
+# Les variables peuvent être utilisées dans des scripts Bash pour stocker des informations, exécuter des commandes, ou effectuer des opérations. Par exemple :
+
+
