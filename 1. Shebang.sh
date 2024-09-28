b11cdcc3f4e7d4b04f1f17e09a9aafb37aa29b97
NiBienvenu <nicibienvenu@gmail.com>
2024-09-28 05:39:13 +0200

    shebang

---
+++ 1. Shebang.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+# THEORIE 
+
+ # la séquence de caractères "#!" est appelée Shebang. 
+ # Elle sert à indiquer au système quel interpréteur de script il doit utiliser pour exécuter le code contenu dans le fichier. 
+ #Cette valeur ne correspond pas forcément à Bash, tout dépend du script. 
+ #Cela est un bon indicateur pour faire fonctionner un script correctement en l'exécutant à l'aide du bon binaire.
+
+
+ # Shebang est divisé en deux partie : # pour SHARP et ! pour BANG
+ 
+
+# En cas d'utlisation du script Python on utilise : #!/bin/python3 Puis on utilise les scripts de python
