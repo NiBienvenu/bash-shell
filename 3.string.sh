c6a4f0aa2cb43a2622cbf6aeca69be52fa7dea18
NiBienvenu <nicibienvenu@gmail.com>
2024-09-29 09:11:28 +0200

    autre fonction de string

--- 3.string.sh
+++ 3.string.sh
@@ -16,3 +16,34 @@ echo ${STR::8} # affiche BIENVENU
 # Affichage a deux position donner
 
 echo ${STR:8:22}
+
+# Extraire le mot dans le chaine de caractère
+
+echo ${STR//COURS/} 
+
+# Les autres fonctions du String
+
+echo ${STR,,} # tout en minuscule
+
+echo ${STR,} # tout en majuscule sauf la première lettre
+
+echo ${#STR} # nombre de caractères
+
+echo ${STR^} # la première lettre en majuscule
+
+echo ${STR^^} # tout en majuscule
+
+echo ${STR: -8} # les 8 derniers caracteres
+
+echo ${STR/S/s} # remplace la première occurrence de S par s
+
+echo ${STR//S/s} # remplace toutes les occurrences de S par s
+
+echo ${STR/S} # supprime la première occurrence de S
+
+echo ${STR//S} # supprime toutes les occurrences de S
+
+
+
+
+
