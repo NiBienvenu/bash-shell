1ba4c921adaff40acaac02693fca710277997496
NiBienvenu <nicibienvenu@gmail.com>
2024-10-01 17:39:46 +0200

    arithimetique fonction

--- 5. arithimetic.sh
+++ 5. arithimetic.sh
@@ -2,3 +2,29 @@
 
 # Arithimetique avec bash shell script
 
+cal=$((7+5))
+echo $cal
+
+# Concaténation
+concat="Hello"
+concat+=" World"
+echo $concat
+ 
+# Arithimetique pour les operations mathematiques
+echo $((10*2))
+echo $((10-2))
+echo $((10/2))
+echo $((10%2))
+
+# Conversion de type
+echo $((10+5))
+echo "10 + 5" | bc
+
+# Arithimetique  avec Expresion
+expr 5 + 9
+
+CAL3=`expr 5 + 9`
+echo $CAL3
+
+# Arithimetique avec bc
+echo "scale=2; 5 + 9" | bc
