51fd0f22c6dbe920a59770edc1b92f54bab5e63c
NiBienvenu <nicibienvenu@gmail.com>
2025-01-01 16:44:34 +0200

    argument dans le bash

---
+++ 7. arguments.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Recevoir des arguments dans le bash
+
+# Premier argument
+echo "$1 les amies"
+
+echo "--------------------------------"
+
+# Second argument
+ echo $2
+
+ echo "--------------------------------"
+ 
+ # Third argument
+
+ echo $(($3+2))
