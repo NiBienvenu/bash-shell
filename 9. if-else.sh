19734b6e42525fad3cb477f1e40fc58332350478
NiBienvenu <nicibienvenu@gmail.com>
2025-01-02 22:53:27 +0200

    if else

---
+++ 9. if-else.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Condition if else dans le bash language
+
+if [ $1 -gt $2 ]; then
+    echo "Le premier argument est plus grand que le deuxieme."
+else
+    echo "Le premier argument est plus petit que le deuxieme."
+fi
