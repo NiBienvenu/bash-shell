011ffd2c38287484668c827f6375c7cb7074268a
NiBienvenu <nicibienvenu@gmail.com>
2025-01-02 22:55:44 +0200

    commentaire de l'exercice

--- 9. if-else.sh
+++ 9. if-else.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# Condition if else dans le bash language
+# Condition if else dans le bash language 
+
+# Comparaison aves des valeurs fornie aux arguments
+# Si le premier argument est supérieur au deuxième, la commande echo affichera "Le premier argument est plus grand que le deuxieme."
+# Sinon, la commande echo affichera "Le premier argument est plus petit que le deuxieme."
+# $1 correspond au premier argument fourni, $2 correspond au deuxième argument fourni.
+# Utilisation :./if-else.sh 5 3
+# Sortie : Le premier argument est plus grand que le deuxieme.
 
 if [ $1 -gt $2 ]; then
     echo "Le premier argument est plus grand que le deuxieme."
