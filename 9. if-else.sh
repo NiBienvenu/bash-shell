0a133f3e470dd602c5afdff8d76144e737e2fe73
Bienvenu Nicimpaye <106779334+NiBienvenu@users.noreply.github.com>
2025-01-05 22:34:26 +0200

    Update 9. if-else.sh

--- 9. if-else.sh
+++ 9. if-else.sh
@@ -7,6 +7,7 @@
 # Sinon, la commande echo affichera "Le premier argument est plus petit que le deuxieme."
 # $1 correspond au premier argument fourni, $2 correspond au deuxième argument fourni.
 # Utilisation :./if-else.sh 5 3
+
 # Sortie : Le premier argument est plus grand que le deuxieme.
 
 if [ $1 -gt $2 ]; then
